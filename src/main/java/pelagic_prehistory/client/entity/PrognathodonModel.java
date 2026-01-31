050d107f7bf5d593cca1aff5a84544ca69af5631
Alex <islicy@outlook.com>
2026-01-31 11:00:28 +0900

    Fix Prognathodon head 360 degree rotation on land

--- src/main/java/pelagic_prehistory/client/entity/PrognathodonModel.java
+++ src/main/java/pelagic_prehistory/client/entity/PrognathodonModel.java
@@ -1,5 +1,6 @@
 package pelagic_prehistory.client.entity;
 
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec2;
 import pelagic_prehistory.entity.Prognathodon;
 import software.bernie.geckolib.core.animation.AnimationState;
@@ -8,6 +9,8 @@ import software.bernie.geckolib.cache.object.GeoBone;
 import java.util.Optional;
 
 public class PrognathodonModel<T extends Prognathodon> extends SimplePitchGeoModel<T> {
+
+    private static final float MAX_HEAD_ROT = 20.0F * Mth.DEG_TO_RAD;
 
     public PrognathodonModel(final String name) {
         super(name);
@@ -34,11 +37,14 @@ public class PrognathodonModel<T extends Prognathodon> extends SimplePitchGeoMod
         if(oHead.isPresent() && oNeck.isPresent()) {
             final GeoBone head = oHead.get();
             final GeoBone neck = oNeck.get();
-            final Vec2 rotations = getHeadRotations(animatable, instanceId, animationState).scale(0.5F);
-            head.setRotX(head.getRotX() + rotations.x);
-            head.setRotY(head.getRotY() + rotations.y);
-            neck.setRotX(neck.getRotX() + rotations.x);
-            neck.setRotY(neck.getRotY() + rotations.y);
+            Vec2 rotations = getHeadRotations(animatable, instanceId, animationState).scale(0.5F);
+            // Clamp rotations to prevent 360 degree spinning on land
+            float clampedX = Mth.clamp(rotations.x, -MAX_HEAD_ROT, MAX_HEAD_ROT);
+            float clampedY = Mth.clamp(rotations.y, -MAX_HEAD_ROT, MAX_HEAD_ROT);
+            head.setRotX(head.getRotX() + clampedX);
+            head.setRotY(head.getRotY() + clampedY);
+            neck.setRotX(neck.getRotX() + clampedX);
+            neck.setRotY(neck.getRotY() + clampedY);
         }
     }
 }
